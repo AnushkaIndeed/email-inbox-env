1f2997a48e4f862ee7af896338fc19046299b732
anushka kohli <anushkakohli@anushkas-MacBook-Air.local>
2026-04-11 03:25:21 +0530

    fix4

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ state = env.reset()
 @app.post("/reset")
 def reset():
     global state
+    print("RESET CALLED")  
     state = env.reset()
     return {
         "done": state.done,
@@ -19,7 +20,8 @@ def reset():
 @app.post("/step")
 def step(action: dict):
     global state
-    
+    print(f"STEP CALLED with action: {action}") 
+
     act = Action(action_type=action.get("action"), confidence=0.8)
     next_state, reward, done = env.step(act)
     state = next_state
